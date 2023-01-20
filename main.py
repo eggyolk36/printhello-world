b2acf21e018a3b33617bb2e7e04a890d65040d1f
Eggyolk <122543465+eggyolk36@users.noreply.github.com>
2023-01-20 09:55:21 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,137 @@
+def on_gesture_eight_g():
+    basic.show_icon(IconNames.CONFUSED)
+input.on_gesture(Gesture.EIGHT_G, on_gesture_eight_g)
 
+def on_button_pressed_a():
+    global mrfishy
+    mrfishy += 1
+    basic.clear_screen()
+    led.plot(diyrobeknotsmelly, mrfishy)
+    if mrfishy >= 4:
+        game.add_score(-1)
+        mrfishy = -1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gesture_free_fall():
+    for index in range(4):
+        music.play_sound_effect(music.builtin_sound_effect(soundExpression.yawn),
+            SoundExpressionPlayMode.IN_BACKGROUND)
+        basic.show_icon(IconNames.SMALL_DIAMOND)
+        basic.show_icon(IconNames.SMALL_SQUARE)
+        basic.show_icon(IconNames.DIAMOND)
+        basic.show_icon(IconNames.SQUARE)
+        basic.show_icon(IconNames.CHESSBOARD)
+input.on_gesture(Gesture.FREE_FALL, on_gesture_free_fall)
+
+def on_gesture_six_g():
+    basic.show_icon(IconNames.CONFUSED)
+input.on_gesture(Gesture.SIX_G, on_gesture_six_g)
+
+def on_gesture_screen_down():
+    basic.show_icon(IconNames.SMALL_HEART)
+    music.play_sound_effect(music.builtin_sound_effect(soundExpression.sad),
+        SoundExpressionPlayMode.IN_BACKGROUND)
+    basic.show_icon(IconNames.HEART)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.show_icon(IconNames.HEART)
+    music.play_sound_effect(music.builtin_sound_effect(soundExpression.sad),
+        SoundExpressionPlayMode.IN_BACKGROUND)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.show_icon(IconNames.HEART)
+input.on_gesture(Gesture.SCREEN_DOWN, on_gesture_screen_down)
+
+def on_button_pressed_ab():
+    for index2 in range(1):
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        # . . . .
+                        # # . . .
+                        . # . . .
+        """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        # # . . .
+                        # # # . .
+                        . . # . .
+        """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        # # # . .
+                        # # # # .
+                        # . . # .
+        """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . # # # .
+                        . # # # #
+                        . # . . #
+        """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . # # #
+                        . . # # #
+                        . . # . .
+        """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . # #
+                        . . . # #
+                        . . . # .
+        """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . #
+                        . . . . #
+                        . . . . #
+        """)
+        basic.pause(100)
+    basic.clear_screen()
+    basic.show_string("Score:")
+    basic.show_number(game.score())
+    basic.pause(500)
+    game.set_score(0)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global diyrobeknotsmelly
+    diyrobeknotsmelly += 1
+    basic.clear_screen()
+    led.plot(diyrobeknotsmelly, mrfishy)
+    if diyrobeknotsmelly >= 5:
+        game.add_score(-1)
+        diyrobeknotsmelly = -1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_logo_down():
+    music.play_melody("E B C5 A B G A F ", 120)
+    basic.show_leds("""
+        . . . . .
+                . # # # .
+                # # # . .
+                # # # # .
+                . # . # .
+    """)
+    music.play_sound_effect(music.builtin_sound_effect(soundExpression.giggle),
+        SoundExpressionPlayMode.UNTIL_DONE)
+    basic.clear_screen()
+input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
+
+mrfishy = 0
+diyrobeknotsmelly = 0
+sprite = None
+diyrobeknotsmelly = 0
+mrfishy = 0
+led.plot(Math.random(), Math.random())
--- main.ts
+++ main.ts
@@ -1,4 +1,113 @@
+input.onGesture(Gesture.EightG, function () {
+    basic.showIcon(IconNames.Confused)
+})
 input.onButtonPressed(Button.A, function () {
+    mrfishy += 1
+    basic.clearScreen()
+    led.plot(diyrobeknotsmelly, mrfishy)
+    if (mrfishy >= 4) {
+        game.addScore(-1)
+        mrfishy = -1
+    }
+})
+input.onGesture(Gesture.FreeFall, function () {
+    for (let index = 0; index < 4; index++) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.InBackground)
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.showIcon(IconNames.SmallSquare)
+        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.Square)
+        basic.showIcon(IconNames.Chessboard)
+    }
+})
+input.onGesture(Gesture.SixG, function () {
+    basic.showIcon(IconNames.Confused)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.SmallHeart)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            # # . . .
+            . # . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # . . .
+            # # # . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # . .
+            # # # # .
+            # . . # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . # # # #
+            . # . . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # #
+            . . # # #
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # #
+            . . . # #
+            . . . # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+        basic.pause(100)
+    }
+    basic.clearScreen()
+    basic.showString("Score:")
+    basic.showNumber(game.score())
+    basic.pause(500)
+    game.setScore(0)
+})
+input.onButtonPressed(Button.B, function () {
+    diyrobeknotsmelly += 1
+    basic.clearScreen()
+    led.plot(diyrobeknotsmelly, mrfishy)
+    if (diyrobeknotsmelly >= 5) {
+        game.addScore(-1)
+        diyrobeknotsmelly = -1
+    }
+})
+input.onGesture(Gesture.LogoDown, function () {
     music.playMelody("E B C5 A B G A F ", 120)
     basic.showLeds(`
         . . . . .
@@ -9,45 +118,9 @@ input.onButtonPressed(Button.A, function () {
         `)
     music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
     basic.clearScreen()
-    music.ringTone(262)
 })
-input.onGesture(Gesture.FreeFall, function () {
-    basic.showString("BOOOOOOOOOOOOOOOOOOOOO")
-    basic.showIcon(IconNames.Sad)
-    music.ringTone(262)
-    basic.showString("HELLO SON, I AM YOUR FATHER")
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    basic.showString("BOOOOOOOOOOOOOOOOOOOOO")
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.SmallHeart)
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
-    basic.showIcon(IconNames.Heart)
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showIcon(IconNames.Heart)
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showIcon(IconNames.Heart)
-})
-input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-    }
-    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.OnceInBackground)
-    music.playMelody("A F E F D G E F ", 120)
-    basic.showIcon(IconNames.Heart)
-})
-input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 4; index++) {
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.InBackground)
-        basic.showIcon(IconNames.SmallDiamond)
-        basic.showIcon(IconNames.SmallSquare)
-        basic.showIcon(IconNames.Diamond)
-        basic.showIcon(IconNames.Square)
-        basic.showIcon(IconNames.Chessboard)
-    }
-})
-input.onGesture(Gesture.TiltRight, function () {
-    basic.showString("BOOOOOOOOOOOOOOOOOOOOO")
-})
+let mrfishy = 0
+let diyrobeknotsmelly = 0
+let sprite = null
+diyrobeknotsmelly = 0
+mrfishy = 0

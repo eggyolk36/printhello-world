19af9157fc0648fda16f33be66fb74a163581406
Eggyolk <122543465+eggyolk36@users.noreply.github.com>
2023-01-23 09:57:11 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 def on_gesture_eight_g():
-    basic.show_icon(IconNames.CONFUSED)
+    basic.show_icon(IconNames.SMALL_HEART)
+    music.play_sound_effect(music.builtin_sound_effect(soundExpression.sad),
+        SoundExpressionPlayMode.IN_BACKGROUND)
+    basic.show_icon(IconNames.HEART)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.show_icon(IconNames.HEART)
+    music.play_sound_effect(music.builtin_sound_effect(soundExpression.sad),
+        SoundExpressionPlayMode.IN_BACKGROUND)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.show_icon(IconNames.HEART)
 input.on_gesture(Gesture.EIGHT_G, on_gesture_eight_g)
 
 def on_button_pressed_a():
-    global mrfishy
-    mrfishy += 1
+    global diyrobeknotsmelly, benga, bill
+    diyrobeknotsmelly += -1
     basic.clear_screen()
     led.plot(diyrobeknotsmelly, mrfishy)
-    if mrfishy >= 4:
-        game.add_score(-1)
-        mrfishy = -1
+    led.plot(bill, benga)
+    if diyrobeknotsmelly <= 0:
+        diyrobeknotsmelly = 4
+        basic.clear_screen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        led.plot(bill, benga)
+    if diyrobeknotsmelly == bill and mrfishy == benga:
+        basic.clear_screen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        benga = randint(0, 5)
+        bill = randint(0, 5)
+        led.plot(bill, benga)
+        game.add_score(1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_gesture_free_fall():
@@ -24,23 +43,77 @@ def on_gesture_free_fall():
 input.on_gesture(Gesture.FREE_FALL, on_gesture_free_fall)
 
 def on_gesture_six_g():
-    basic.show_icon(IconNames.CONFUSED)
+    music.play_melody("E B C5 A B G A F ", 120)
+    basic.show_leds("""
+        . . . . .
+                . # # # .
+                # # # . .
+                # # # # .
+                . # . # .
+    """)
+    music.play_sound_effect(music.builtin_sound_effect(soundExpression.giggle),
+        SoundExpressionPlayMode.UNTIL_DONE)
+    basic.clear_screen()
 input.on_gesture(Gesture.SIX_G, on_gesture_six_g)
 
-def on_gesture_screen_down():
-    basic.show_icon(IconNames.SMALL_HEART)
-    music.play_sound_effect(music.builtin_sound_effect(soundExpression.sad),
-        SoundExpressionPlayMode.IN_BACKGROUND)
-    basic.show_icon(IconNames.HEART)
-    basic.show_icon(IconNames.SMALL_HEART)
-    basic.show_icon(IconNames.HEART)
-    music.play_sound_effect(music.builtin_sound_effect(soundExpression.sad),
-        SoundExpressionPlayMode.IN_BACKGROUND)
-    basic.show_icon(IconNames.SMALL_HEART)
-    basic.show_icon(IconNames.HEART)
-input.on_gesture(Gesture.SCREEN_DOWN, on_gesture_screen_down)
+def on_gesture_screen_up():
+    global rockpaper, benga, bill
+    rockpaper = randint(1, 3)
+    basic.show_string("rock paper scissors SHOE!")
+    if rockpaper == 1:
+        basic.clear_screen()
+        rockpaper = randint(1, 3)
+        if rockpaper == 2:
+                basic.clear_screen()
+                rockpaper = randint(1, 3)
+                if rockpaper == 3:
+                        basic.clear_screen()
+                        rockpaper = randint(1, 3)
+
+
+input.on_gesture(Gesture.SCREEN_UP, on_gesture_screen_up)
 
 def on_button_pressed_ab():
+    global mrfishy, benga, bill
+    mrfishy += 1
+    basic.clear_screen()
+    led.plot(diyrobeknotsmelly, mrfishy)
+    led.plot(bill, benga)
+    if mrfishy >= 5:
+        mrfishy = 0
+        basic.clear_screen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        led.plot(bill, benga)
+    if diyrobeknotsmelly == bill and mrfishy == benga:
+        basic.clear_screen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        benga = randint(0, 5)
+        bill = randint(0, 5)
+        led.plot(bill, benga)
+        game.add_score(1)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global diyrobeknotsmelly, benga, bill
+    diyrobeknotsmelly += 1
+    basic.clear_screen()
+    led.plot(diyrobeknotsmelly, mrfishy)
+    led.plot(bill, benga)
+    if diyrobeknotsmelly >= 5:
+        diyrobeknotsmelly = 0
+        basic.clear_screen()
+        led.plot(bill, benga)
+        led.plot(diyrobeknotsmelly, mrfishy)
+    if diyrobeknotsmelly == bill and mrfishy == benga:
+        basic.clear_screen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        benga = randint(0, 5)
+        bill = randint(0, 5)
+        led.plot(bill, benga)
+        game.add_score(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
     for index2 in range(1):
         basic.show_leds("""
             . . . . .
@@ -103,35 +176,20 @@ def on_button_pressed_ab():
     basic.show_number(game.score())
     basic.pause(500)
     game.set_score(0)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
-def on_button_pressed_b():
-    global diyrobeknotsmelly
-    diyrobeknotsmelly += 1
-    basic.clear_screen()
-    led.plot(diyrobeknotsmelly, mrfishy)
-    if diyrobeknotsmelly >= 5:
-        game.add_score(-1)
-        diyrobeknotsmelly = -1
-input.on_button_pressed(Button.B, on_button_pressed_b)
+def on_gesture_three_g():
+    basic.show_icon(IconNames.CONFUSED)
+input.on_gesture(Gesture.THREE_G, on_gesture_three_g)
 
-def on_gesture_logo_down():
-    music.play_melody("E B C5 A B G A F ", 120)
-    basic.show_leds("""
-        . . . . .
-                . # # # .
-                # # # . .
-                # # # # .
-                . # . # .
-    """)
-    music.play_sound_effect(music.builtin_sound_effect(soundExpression.giggle),
-        SoundExpressionPlayMode.UNTIL_DONE)
-    basic.clear_screen()
-input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
-
+rockpaper = 0
+bill = 0
 mrfishy = 0
 diyrobeknotsmelly = 0
-sprite = None
-diyrobeknotsmelly = 0
+benga = 0
+diusy = 0
+benga = randint(0, 5)
+diyrobeknotsmelly = randint(0, 5)
 mrfishy = 0
-led.plot(Math.random(), Math.random())
+bill = 0
+rockpaper = 0
--- main.ts
+++ main.ts
@@ -1,13 +1,31 @@
 input.onGesture(Gesture.EightG, function () {
-    basic.showIcon(IconNames.Confused)
+    basic.showIcon(IconNames.SmallHeart)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.A, function () {
-    mrfishy += 1
+    diyrobeknotsmelly += -1
     basic.clearScreen()
     led.plot(diyrobeknotsmelly, mrfishy)
-    if (mrfishy >= 4) {
-        game.addScore(-1)
-        mrfishy = -1
+    led.plot(bill, benga)
+    if (diyrobeknotsmelly <= 0) {
+        diyrobeknotsmelly = 4
+        basic.clearScreen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        led.plot(bill, benga)
+    }
+    if (diyrobeknotsmelly == bill && mrfishy == benga) {
+        basic.clearScreen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        benga = randint(0, 5)
+        bill = randint(0, 5)
+        led.plot(bill, benga)
+        game.addScore(1)
     }
 })
 input.onGesture(Gesture.FreeFall, function () {
@@ -21,19 +39,58 @@ input.onGesture(Gesture.FreeFall, function () {
     }
 })
 input.onGesture(Gesture.SixG, function () {
-    basic.showIcon(IconNames.Confused)
-})
-input.onGesture(Gesture.ScreenDown, function () {
-    basic.showIcon(IconNames.SmallHeart)
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
-    basic.showIcon(IconNames.Heart)
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showIcon(IconNames.Heart)
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showIcon(IconNames.Heart)
+    music.playMelody("E B C5 A B G A F ", 120)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # # # . .
+        # # # # .
+        . # . # .
+        `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
+    mrfishy += 1
+    basic.clearScreen()
+    led.plot(diyrobeknotsmelly, mrfishy)
+    led.plot(bill, benga)
+    if (mrfishy >= 5) {
+        mrfishy = 0
+        basic.clearScreen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        led.plot(bill, benga)
+    }
+    if (diyrobeknotsmelly == bill && mrfishy == benga) {
+        basic.clearScreen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        benga = randint(0, 5)
+        bill = randint(0, 5)
+        led.plot(bill, benga)
+        game.addScore(1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    diyrobeknotsmelly += 1
+    basic.clearScreen()
+    led.plot(diyrobeknotsmelly, mrfishy)
+    led.plot(bill, benga)
+    if (diyrobeknotsmelly >= 5) {
+        diyrobeknotsmelly = 0
+        basic.clearScreen()
+        led.plot(bill, benga)
+        led.plot(diyrobeknotsmelly, mrfishy)
+    }
+    if (diyrobeknotsmelly == bill && mrfishy == benga) {
+        basic.clearScreen()
+        led.plot(diyrobeknotsmelly, mrfishy)
+        benga = randint(0, 5)
+        bill = randint(0, 5)
+        led.plot(bill, benga)
+        game.addScore(1)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 1; index++) {
         basic.showLeds(`
             . . . . .
@@ -98,29 +155,57 @@ input.onButtonPressed(Button.AB, function () {
     basic.pause(500)
     game.setScore(0)
 })
-input.onButtonPressed(Button.B, function () {
-    diyrobeknotsmelly += 1
-    basic.clearScreen()
-    led.plot(diyrobeknotsmelly, mrfishy)
-    if (diyrobeknotsmelly >= 5) {
-        game.addScore(-1)
-        diyrobeknotsmelly = -1
+input.onGesture(Gesture.LogoDown, function () {
+    rockpaper = randint(1, 3)
+    basic.showString("rock paper scissors SHOE!")
+    if (rockpaper == 1) {
+        basic.clearScreen()
+        rockpaper = randint(1, 3)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.showString("ROCK!")
+        if (rockpaper == 2) {
+            basic.clearScreen()
+            rockpaper = randint(1, 3)
+            basic.showLeds(`
+                . # . # .
+                . . # . .
+                . # . # .
+                # # . # #
+                # # . # #
+                `)
+            basic.showString("SCISSORS!")
+            if (rockpaper == 3) {
+                basic.clearScreen()
+                rockpaper = randint(1, 3)
+                basic.showLeds(`
+                    . . . . .
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    `)
+                basic.showString("PAPER!")
+            }
+        }
     }
 })
-input.onGesture(Gesture.LogoDown, function () {
-    music.playMelody("E B C5 A B G A F ", 120)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        # # # . .
-        # # # # .
-        . # . # .
-        `)
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
-    basic.clearScreen()
+input.onGesture(Gesture.ThreeG, function () {
+    basic.showIcon(IconNames.Confused)
 })
+let rockpaper = 0
+let bill = 0
 let mrfishy = 0
 let diyrobeknotsmelly = 0
-let sprite = null
-diyrobeknotsmelly = 0
+let benga = 0
+let diusy = 0
+benga = randint(0, 5)
+diyrobeknotsmelly = randint(0, 5)
 mrfishy = 0
+bill = 0
+rockpaper = randint(1, 3)
